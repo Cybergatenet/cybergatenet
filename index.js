2593a39cfae44bf94f7b4e1d2bf562541cce02ca
cybergate <cybergatenet@yahoo.com>
2020-08-26 14:14:27 -0700

    added Admin pages and error pages

--- index.js
+++ index.js
@@ -9,11 +9,20 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
+// Login route
 app.get('/login', (req, res) => {
     app.use(express.static(path.join(__dirname, 'client')));
     app.use(express.static(path.join(__dirname, 'client/Neumorphic Login')));
     // res.send('Hello World from Cybergate');
     res.sendFile(path.join(__dirname, 'client/Neumorphic Login', 'index.html'));    
+});
+
+// Admin side route
+app.get('/login', (req, res) => {
+    // app.use(express.static(path.join(__dirname, 'client')));
+    app.use(express.static(path.join(__dirname, 'AdminDashBoard')));
+    // res.send('Hello World from Cybergate');
+    res.sendFile(path.join(__dirname, 'AdminDashBoard', 'index.html'));    
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
