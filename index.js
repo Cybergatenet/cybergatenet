00a7671cf1b0d0c2ef30734d804cddef02e70b5e
cybergate <cybergatenet@yahoo.com>
2020-08-28 09:34:17 -0700

    fied few bugs

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get('/login', (req, res) => {
     app.use(express.static(path.join(__dirname, 'client/Neumorphic Login')));
     res.sendFile(path.join(__dirname, 'client/Neumorphic Login', 'index.html'));    
 });
+// Sign up route
+app.get('/signup', (req, res) => {
+    app.use(express.static(path.join(__dirname, 'client')));
+    app.use(express.static(path.join(__dirname, 'client/sign_up')));
+    res.sendFile(path.join(__dirname, 'client/sign_up', 'attractive-signup-UI-design.html'));    
+});
 
 // Admin side route
 app.get('/CyberAdmin', (req, res) => {
